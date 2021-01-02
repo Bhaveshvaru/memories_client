e23bf3ca1c7370cd4552599f154c803ca90fe042
bhaveshvaru <bhavesh.varu97@gmail.com>
2021-01-02 23:26:55 +0530

    added image and scafoldded app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import memories from "./image/memories.jpg"
+
+const App =()=>{
+ return(
+   <Container maxWidth="lg">
+       <AppBar position="static" color="inherit"> 
+       <Typography variant="h2" align="center"> Memories</Typography>
+        <img src={memories} alt="memories" height="60"/>
+        </AppBar>
+    </Container>
+
+ )
 }
-
-export default App;
+ export default App;
